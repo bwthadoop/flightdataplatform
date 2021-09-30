37864fe30e61825efa48f2df52817569cc88ff44
Saurabh5757 <surbh8808@gmail.com>
2021-09-30 16:29:24 +0530

    Changes in Code

---
+++ saurabh_tembre/scripts/hive_ddl.sh
@@ -0,0 +1,41 @@
+hive -e "create database if not exists ${1}"
+
+#create hive external table
+hive -e " create table if not exists ${1}.airports_temp
+         (
+         airport_id int,
+         city string,
+         state string,
+         name string
+         )
+         row format delimited
+         fields terminated by ','
+         lines terminated by '/n'
+         "
+
+hive -e " create external table if not exists ${1}.airports
+         (
+         airport_id int,
+         city string,
+         name string
+         )
+         partitioned by(state string)
+         row format delimited
+         fields terminated by ','
+         lines terminated by '/n'
+         location '${target_base_path}/${2}'
+         "
+read -p "Enter column for Partition: " partition
+$4=partition
+
+hive -e " set hive.exec.dynamic.partition=true;
+          set hive.exec.dynamic.partition.mode=nonstrict;
+          set hive.support.quoted.identifiers=none;
+          insert overwrite ${1}.${2} partition(${4}) select \`(${4})?+.+\`,${4} from ${1}.${airport_temp};
+          "
+if [ $? -ne 0 ]
+then
+  echo -e ${ERROR} " Failed to Insert into Hive Partition Table"
+    exit 1
+fi
+  echo -e ${INFO}" Record Inserted into Hive Partition Table"
