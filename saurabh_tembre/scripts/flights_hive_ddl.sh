791301c3ba135fb77dcf9ddb2e9d64935c4230de
Saurabh5757 <surbh8808@gmail.com>
2021-09-30 17:03:44 +0530

    Changes in Code

--- saurabh_tembre/scripts/flights_hive_ddl.sh
+++ saurabh_tembre/scripts/flights_hive_ddl.sh
@@ -28,3 +28,17 @@ hive -e " create external table if not exists ${1}.flights
          lines terminated by '/n'
          location '${target_base_path}/${2}'
          "
+read -p "Enter column for Partition: " partition
+$3=partition
+
+hive -e " set hive.exec.dynamic.partition=true;
+          set hive.exec.dynamic.partition.mode=nonstrict;
+          set hive.support.quoted.identifiers=none;
+          insert overwrite ${1}.${2} partition(${3}) select \`(${3})?+.+\`,${3} from ${1}.${flights_temp};
+          "
+if [ $? -ne 0 ]
+then
+  echo -e ${ERROR} " Failed to Insert into Hive Partition Table"
+    exit 1
+fi
+  echo -e ${INFO}" Record Inserted into Hive Partition Table"
